7b54375933029b5ac7b29be346a12bf7d0c139ea
Matei-Marius Micu <mmicu@cloudbasesolutions.com>
2017-03-16 13:10:27 +0200

    Implementeaza main

--- Licenta/Anul_II/Semestrul_II/PA/Seminarii/seminar_4/Main.java
+++ Licenta/Anul_II/Semestrul_II/PA/Seminarii/seminar_4/Main.java
@@ -1,11 +1,40 @@
+import java.util.*;
 class Main
 {
-    public static void Main(String args)
+    public static void main(String[] args)
     {
+        ArrayList<Command> commands = new ArrayList<Command>();
+        // commands.add(new)
         /* main 
          * - are o lista cu commands 
          * - apeleaza fiecare comanda in functie de cum prefix
          * */
+        String inp = "";
+        Scanner sc = new Scanner(System.in);
+        while(!inp.equals("q"))
+        {
+            System.out.print(">> ");
+            inp = sc.nextLine();
+            boolean found = false;
+            for(Command cmd : commands)
+            {
+                if(inp.startsWith(cmd.getPrefix()))
+                {
+                    found = true;
+                    try{
+                        cmd.run(inp);
+                    }
+                    catch(BaseException exp)
+                    {
+                        System.out.println("Exception: " + exp.toString());
+                    }
+                }
+            }
 
+            if(!found && !inp.equals("q"))
+            {
+                System.out.println("Command '" + inp + "' not found!");
+            }
+        }
     }
 }
